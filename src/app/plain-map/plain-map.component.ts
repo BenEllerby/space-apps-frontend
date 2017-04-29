50a46fb5681651d281b1cf3ed18a504339c326ca
Shivani Joshi <SHIVANIJ@uk.ibm.com>
2017-04-29 20:39:42 +0100

    Fixing image bug

--- src/app/plain-map/plain-map.component.ts
+++ src/app/plain-map/plain-map.component.ts
@@ -11,10 +11,10 @@ export class PlainMapComponent implements OnInit {
   mockResponse = JSON.parse(this.markerinfoService.getData());
   lat: number = this.mockResponse.markers[1].lat;
   lng: number = this.mockResponse.markers[1].lng;
-  markertext = this.mockResponse.markers[1].markerPicURL;
 
   constructor(private markerinfoService: MarkerInfoService) { }
 
   ngOnInit() {
+    console.log(this.mockResponse.markers);
   }
 }
